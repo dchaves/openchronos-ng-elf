cd19411097ebf871bcf36d51f7a9182872083be7
David Chaves <dchaves@gradiant.org>
2020-02-20 11:32:09 +0100

    Testing actions

---
+++ drivers/rtca_now.h
@@ -0,0 +1,12 @@
+
+#ifndef __RTCA_NOW_H__
+#define __RTCA_NOW_H__
+
+#define COMPILE_YEAR 2020
+#define COMPILE_MON 2
+#define COMPILE_DAY 20
+#define COMPILE_DOW 4
+#define COMPILE_HOUR 10
+#define COMPILE_MIN 29
+
+#endif
